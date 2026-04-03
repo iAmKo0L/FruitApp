9f80045ee7684b97aa1899b00721b461f47e28ef
huytrandev <huytq1226@gmail.com>
2026-04-03 14:33:31 +0700

    add trang activity danh mục san pham

---
+++ app/src/main/java/com/example/fruitapp/activities/CategoryListActivity.java
@@ -0,0 +1,43 @@
+package com.example.fruitapp.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.studentmanager.R;
+import com.example.studentmanager.adapters.CategoryAdapter;
+import com.example.studentmanager.dal.AppDB;
+import com.example.studentmanager.entities.Category;
+
+import java.util.List;
+
+public class CategoryListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_list);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("Danh Mục Sản Phẩm");
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerCategories);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        List<Category> categories = AppDB.getInstance(this).categoryDAO().getAll();
+        recyclerView.setAdapter(new CategoryAdapter(this, categories));
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+}
